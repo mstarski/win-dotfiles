556ae81cc3cf4e9562c4f6ddb28e574c091898b4
Michał Starski <michals@pixelhat.pl>
2023-06-12 17:26:56 +0200

    Update 12.06.2023

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,7 +6,7 @@ local schemastore = require("schemastore")
 local ts = require("typescript")
 
 lsp.ensure_installed({ "tsserver", "lua_ls" })
-lsp.set_sign_icons({ error = "✘", warn = "▲", hint = "⚑", info = "»" })
+lsp.set_sign_icons({ error = "🚨", warn = "🟡", hint = "🔵", info = "🟢" })
 lsp.setup({ sources = { { name = "nvim_lsp" }, { name = "nvim_lua" } } })
 
 lsp.skip_server_setup({ "tsserver" })
@@ -19,17 +19,7 @@ end)
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
 
 -- TYPESCRIPT
-ts.setup({
-	server = {
-		on_attach = function(client, bufnr)
-			-- You can find more commands in the documentation:
-			-- https://github.com/jose-elias-alvarez/typescript.nvim#commands
-			if client.name == "tsserver" then
-				client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
-			end
-		end,
-	},
-})
+ts.setup({})
 
 -- JSON
 lspconfig.jsonls.setup({
@@ -80,3 +70,4 @@ vim.keymap.set("n", "<M-w>", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<M-S-r>", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>oi", ts.actions.organizeImports)
 vim.keymap.set("n", "<leader>ru", ts.actions.removeUnused)
+vim.keymap.set("n", "<leader>am", ts.actions.addMissingImports)
