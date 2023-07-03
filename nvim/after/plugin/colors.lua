c5a8562a07d419079cafe060d69f5849d45e1238
Michał Starski <michal.starski@proton.me>
2023-07-03 23:55:41 +0200

    Update 03.07.2023

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -48,8 +48,11 @@ require("rose-pine").setup({
 	},
 })
 
-vim.o.background = "light"
-
 -- Set colorscheme after options
--- vim.cmd("colorscheme rose-pine")
-vim.cmd("colorscheme tokyonight-day")
+vim.cmd("colorscheme rose-pine")
+-- vim.cmd("colorscheme tokyonight")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
