f6b84b194b1ae9602f17f7f71636ee4027819568
Michał Starski <michal.starski@proton.me>
2023-07-04 23:05:35 +0200

    Update 07.04.2023

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,2 +1,7 @@
 vim.cmd([[ imap <silent><script><expr> <Right> copilot#Accept("\<CR>") ]])
 vim.cmd([[ let g:copilot_no_tab_map = v:true]])
+
+vim.api.nvim_set_var("copilot_filetypes", {
+	["dap-repl"] = false,
+	["dapui_watches"] = false,
+})
--- nvim/after/plugin/test.lua
+++ nvim/after/plugin/test.lua
@@ -1,1 +1,39 @@
-require("nvim-test").setup()
+local neotest = require("neotest")
+
+neotest.setup({
+	adapters = {
+		require("neotest-dotnet")({
+			dap = { justMyCode = false },
+		}),
+		require("neotest-jest")({
+			jestCommand = "npm test --",
+			cwd = function()
+				return vim.fn.getcwd()
+			end,
+		}),
+	},
+})
+
+vim.keymap.set("n", "<leader>tr", function()
+	neotest.run.run()
+end)
+
+vim.keymap.set("n", "<leader>to", function()
+	neotest.output.open({ last_run = true, enter = true })
+end)
+
+vim.keymap.set("n", "<leader>tp", function()
+	neotest.output_panel.toggle()
+end)
+
+vim.keymap.set("n", "<leader>tf", function()
+	neotest.run.run(vim.fn.expand("%"))
+end)
+
+vim.keymap.set("n", "<leader>ts", function()
+	neotest.summary.toggle()
+end)
+
+vim.keymap.set("n", "<leader>tw", function()
+	neotest.watch.toggle()
+end)
