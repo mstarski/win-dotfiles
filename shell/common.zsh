276efd2582bd1c33a8451ad7df172b510a10d874
Michał Starski <michal.starski@justjoin.it>
2023-07-31 17:05:48 +0200

    chore: commit save point

--- shell/common.zsh
+++ shell/common.zsh
@@ -1,15 +1,17 @@
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-export PURE_NODE_ENABLED=0
+PURE_NODE_ENABLED=0
 PURE_PROMPT_SYMBOL="$"
-
 ZSH_THEME="lambda-pure"
+ZSH_THEME_AWS_REGION_PREFIX="🌍 "
+ZSH_THEME_AWS_REGION_SUFFIX=""
 
 plugins=(
 	git
 	zsh-autosuggestions
-    docker
+  docker
+  aws
 )
 
 # Preferred editor for local and remote sessions
