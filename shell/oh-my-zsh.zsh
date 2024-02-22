3c19699ab98e21b75af03a37d29f5a1bb03248f3
Michał Starski <michals@pixelhat.pl>
2024-02-22 16:41:26 +0100

    chore: commit save point

--- shell/oh-my-zsh.zsh
+++ shell/oh-my-zsh.zsh
@@ -1,9 +1,17 @@
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# Pure: https://github.com/sindresorhus/pure#oh-my-zsh
+# (manual install)
+if [ -d $HOME/.zsh/pure ]; then 
+  fpath+=($HOME/.zsh/pure)
+
+  autoload -U promptinit; promptinit
+  prompt pure
+fi
+
 PURE_NODE_ENABLED=0
-PURE_PROMPT_SYMBOL="$"
-ZSH_THEME="lambda-pure"
+PURE_PROMPT_SYMBOL="λ"
 ZSH_THEME_AWS_REGION_PREFIX="🌍 "
 ZSH_THEME_AWS_REGION_SUFFIX=""
 
